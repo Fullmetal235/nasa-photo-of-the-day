402c9ae9e7f12c4a58ec43484fdcb8556142eb9f
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-07-17 16:37:36 -0500

    Creating components.

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Nav from './nav.js'
+
+ export function Header() {
+    return <header>
+        <h1>NASA photo of the day!</h1>
+        <Nav />
+        </header>
+}
---
+++ components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ export function Nav() {
+
+     return <nav>
+        <a href='#'>Home</a>
+        <a href='#'>Services</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
+    </nav>
+}
