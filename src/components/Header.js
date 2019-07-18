746036b1846ed2b14845e5e8f8ac8cd04b942251
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-07-17 19:33:15 -0500

    axios install.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Nav from './nav.js'
+import Nav from './Nav.js'
 
- export function Header() {
+ function Header() {
     return <header>
         <h1>NASA photo of the day!</h1>
         <Nav />
